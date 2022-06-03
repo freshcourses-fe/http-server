45ff37403a6092b6458727b6b099a0d9842795b9
Kirill <freshjs.ftd.@gmail.com>
2022-06-03 20:54:39 +0300

    Add post request with middlwares

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const fs = require('fs').promises
 const path = require('path')
+const yup = require('yup')
 const app = express()
+
+const bodyParser = express.json()
 
 app.get(
   '/',
   (req, res, next) => {
     console.log('middleware')
-    if(true) {
+    if (true) {
       next()
     }
   },
@@ -20,9 +23,32 @@ app.get(
   }
 )
 
-// app.post('/', (req, res, next) => {
+const LOGIN_SCHEMA = yup.object({
+  login: yup.string().required(),
+  password: yup.string().matches(/[A-Za-z0-9]{4,32}/).required()
+})
 
-// })
+const validateUser = (req, res, next) => {
+  const user = req.body
+
+  console.log(user)
+
+  LOGIN_SCHEMA.validate(user).then(user=> {
+    req.user = user
+    next()
+  }).catch(error => {
+    res.status(400).send(error)
+  })
+}
+
+app.post(
+  '/',
+  bodyParser,
+  validateUser,
+  (req, res, next) => {
+    res.send(req.user)
+  }
+)
 
 app.listen(5000, () => {
   console.log('app is running')
